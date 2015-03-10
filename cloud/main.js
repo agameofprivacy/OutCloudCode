ec165a2e86b603626fd48597dca368c723be7982
Eddie Chen <agameofprivacy@gmail.com>
2015-03-10 13:39:48 -0400

    Notification now sent to relevant user

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,41 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+
+
+Parse.Cloud.define("challengeOfDifficulty", function(request, response) {
+  var query = new Parse.Query("Challenge");
+  query.equalTo("difficulty", request.params.difficulty);
+  query.find({
+    success: function(results) {
+      response.success(results.length);
+    },
+    error: function() {
+      response.error("challenge lookup failed");
+    }
+  });
+});
+
+Parse.Cloud.afterSave("Notification", function(request) {
+  // Our "Notification" class has a "text" key with the body of the comment itself
+  var notificationType = request.object.get('type');
+ 
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('deviceType', 'ios');
+  pushQuery.equalTo('currentUser', request.object.get('receiver'));
+    
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query
+    data: {
+      alert: "New notification of type " + notificationType
+    }
+  }, {
+    success: function() {
+      // Push was successful
+    },
+    error: function(error) {
+      throw "Got an error " + error.code + " : " + error.message;
+    }
+  });
+});
